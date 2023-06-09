a22a8ad10b43ce348866572fa34616670e880210
Manukumar N M <manunm.jps@gmail.com>
2023-06-09 13:21:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ from setuptools import find_packages, setup
 from openwisp_firmware_upgrader import get_version
 
 # TODO: change this when next version of openwisp_controller is released
-controller = 'https://github.com/Manukumar-M/openwisp-controller/tarball/master'
+controller = 'https://github.com/ManukumarNM/openwisp-controller/tarball/master'
 # TODO: change this when next version of openwisp_utils is released
-utils = 'https://github.com/Manukumar-M/openwisp-utils/tarball/master'
+utils = 'https://github.com/ManukumarNM/openwisp-utils/tarball/master'
 
 if sys.argv[-1] == 'publish':
     # delete any *.pyc, *.pyo and __pycache__
